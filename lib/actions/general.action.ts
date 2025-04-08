b47514d6db978dd658004a976239971b9596c35b
André C <35017477+AndreeChamoun@users.noreply.github.com>
2025-04-08 17:54:27 +0200

    Fixing bugs

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -74,6 +74,24 @@ export async function getInterviewById(
   return interview.data() as Interview | null;
 }
 
+export async function getFeedbackByInterviewId(
+  params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+  const { interviewId, userId } = params;
+
+  const querySnapshot = await db
+    .collection('feedback')
+    .where('interviewId', '==', interviewId)
+    .where('userId', '==', userId)
+    .limit(1)
+    .get();
+
+  if (querySnapshot.empty) return null;
+
+  const feedbackDoc = querySnapshot.docs[0];
+  return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+}
+
 export async function getLatestInterviews(
   params: GetLatestInterviewsParams
 ): Promise<Interview[] | null> {
@@ -106,26 +124,4 @@ export async function getInterviewsByUserId(
     id: doc.id,
     ...doc.data(),
   })) as Interview[];
-}
-
-export async function getFeedbackByInterviewId(
-  params: GetFeedbackByInterviewIdParams
-): Promise<Feedback | null> {
-  const { interviewId, userId } = params;
-
-  const feedback = await db
-    .collection('feedback')
-    .where('interviewId', '==', interviewId)
-    .where('userId', '==', userId)
-    .limit(1)
-    .get();
-
-  if (feedback.empty) return null;
-
-  const feedbackDoc = feedback.docs[0];
-
-  return {
-    id: feedbackDoc.id,
-    ...feedbackDoc.data(),
-  } as Feedback;
 }

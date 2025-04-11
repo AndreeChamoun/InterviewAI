dd8531e8fbde7cb24be59e637c1b2f200d84a4f6
André C <35017477+AndreeChamoun@users.noreply.github.com>
2025-04-11 15:11:22 +0200

    Updating text

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -7,7 +7,14 @@ const page = async () => {
   return (
     <>
       <h3>Interview Generation</h3>
-
+      <h1>
+        How it works: You press the call button and the AI
+        interviewer picks up the call. It will then ask you a few
+        questions on{' '}
+        <span style={{ textDecoration: 'underline' }}>how</span> you
+        would like to tailor your interview. Which you then can
+        choose from the homepage.
+      </h1>
       <Agent
         userName={user?.name}
         userId={user?.id}
